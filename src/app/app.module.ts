cc417455b8783dcecc93611b5099ab01da0d0b53
harish0796 <harish0796@gmail.com>
2019-08-16 14:50:18 +0530

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,10 +23,6 @@ import {NotFoundComponent} from './not-found/not-found.component';
       },{
         path:'list',
         component:ListComponent
-      },
-      {
-        path:'**',
-        component:NotFoundComponent
       }
     ])
   ],
