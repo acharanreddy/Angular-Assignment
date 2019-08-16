90a37621e70bb9500cf9c76e64cb8a2d44db1f69
krishnamurthy98 <krishnaraghu98@gmail.com>
2019-08-16 12:37:40 +0530

    2nd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
