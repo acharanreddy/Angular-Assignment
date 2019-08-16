eced05c846220fccdfb06aedbbc6049dc12e9441
krishnamurthy98 <krishnaraghu98@gmail.com>
2019-08-16 16:36:07 +0530

    3rd

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -5,11 +5,63 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  courses = [
+    {
+      name:"Java 8", 
+      id:1,
+      imgUrl:'../assets/angular.png', 
+      price:8000,
+      offer:true,
+      duration:10,
+      participants:0
+    },
+    {
+      name:"Maven", 
+      id:2, 
+      price:4000,
+      offer:false,
+      imgUrl:'../assets/angular.png',
+      duration:2,
+      participants:0
+    },
+    {
+      name:"Spring Boot", 
+      id:3, 
+      offer:true,
+      price:10000,
+      imgUrl:'../assets/angular.png',
+      duration:8,
+      participants:0
+    },
+    {
+      name:"Hibernate", 
+      id:4, 
+      offer:true,
+      imgUrl:'../assets/angular.png',
+      price:8000,
+      duration:4,
+      participants:0
+    },
+    {
+      name:"JPA", 
+      id:5, 
+      offer:false,
+      price:8000,
+      imgUrl:'../assets/angular.png',
+      duration:4,
+      participants:0
+    },
+    {
+      name:"Angular", 
+      id:6, 
+      offer:false,
+      price:10000,
+      duration:4,
+      imgUrl:'../assets/angular.png',
+      participants:0
+    }
+  ];
 
 }
